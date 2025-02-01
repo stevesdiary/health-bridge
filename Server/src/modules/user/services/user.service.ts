261cb2407235495589caa011086354b07aaf40fd
Stephen Oyeyemi <94493468+stevesdiary@users.noreply.github.com>
2025-02-02 00:26:16 +0100

    Add user service with CRUD operations and caching support

---
+++ Server/src/modules/user/services/user.service.ts
@@ -0,0 +1,122 @@
+import { customAlphabet } from "nanoid";
+import bcrypt from "bcrypt";
+
+import { getFromRedis, saveToRedis } from "../../../core/redis";
+import { CreationAttributes } from "sequelize";
+import { User } from "../models/user.model";
+import { UserResponseData } from "../../types/type";
+
+export const getAllUsers = async (): Promise<UserResponseData> => {
+  try {
+    let key = "fetch:allUsers";
+    let fetchUsers: string | null = await getFromRedis(key);
+    if (fetchUsers) {
+      return {
+        statusCode: 200,
+        status: "success",
+        message: "Users fetched from cache",
+        data: JSON.parse(fetchUsers),
+      };
+    }
+    const users = await User.findAll({
+      attributes: {
+        exclude: ["password", "createdAt", "updatedAt"],
+      },
+    });
+
+    if (users && users.length > 0) {
+      await saveToRedis(key, JSON.stringify(users), 300);
+      console.log("Users saved to redis");
+      return {
+        statusCode: 200,
+        status: "success",
+        message: "Users fetched from database",
+        data: users,
+      };
+    }
+
+    return {
+      statusCode: 404,
+      status: "fail",
+      message: "No user found",
+      data: [],
+    };
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const getOneUser = async (id: string) => {
+  try {
+    const user = await User.findByPk(id, {
+      attributes: {
+        exclude: ["password", "createdAt", "updatedAt"],
+      },
+    });
+    if (!user) {
+      return {
+        statusCode: 404,
+        status: "fail",
+        message: "User not found",
+        data: [],
+      };
+    }
+    return {
+      statusCode: 200,
+      status: "success",
+      message: "User found",
+      data: user,
+    };
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const updateUser = async (
+  id: string,
+  userData: CreationAttributes<User>
+) => {
+  try {
+    const user = await User.findByPk(id);
+    if (!user) {
+      return {
+        statusCode: 404,
+        status: "fail",
+        message: "User not found",
+        data: [],
+      };
+    }
+    const updatedUser = await user.update(userData);
+    return {
+      statusCode: 200,
+      status: "success",
+      message: "User updated",
+      data: updatedUser,
+    };
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const deleteUser = async (id: string) => {
+  try {
+    const deletedRow = await User.destroy({ where: { id }});
+    if (!deletedRow || deletedRow === 0) {
+      return {
+        statusCode: 404,
+        status: "fail",
+        message: "User not found",
+        data: [],
+      };
+    }
+
+    return {
+      statusCode: 200,
+      status: "success",
+      message: "User deleted",
+      data: deletedRow,
+    };
+  } catch (error) {
+    throw error;
+  }
+}
