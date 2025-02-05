0de2bad3fae06100efc82fcc31d6444760b956db
Stephen Oyeyemi <94493468+stevesdiary@users.noreply.github.com>
2025-02-05 13:50:08 +0100

    Refactor registration route to use ExpressRequest for improved type clarity

--- Server/src/modules/user/routes/registration.route.ts
+++ Server/src/modules/user/routes/registration.route.ts
@@ -1,10 +1,9 @@
-import { Router, Request, Response } from 'express';
+import { Router, Request as ExpressRequest, Response } from 'express';
 
 import userRegistration from '../controllers/register.user';
 import userController from '../controllers/user.controller';
 import { TypedRequest } from '../../types/type';
 import { checkRole } from '../../../middlewares/authorisation';
-import { deleteUser } from '../../../../../Server/src/modules/user/services/user.service';
 
 
 const userRouter = Router();
@@ -13,19 +12,19 @@ userRouter.post("/register", async (req: TypedRequest, res: Response) => {
   await userRegistration.register(req, res);
 });
 
-userRouter.post("/verify", async (req: Request, res: Response) => {
+userRouter.post("/verify", async (req: ExpressRequest, res: Response) => {
   await userRegistration.verifyUser(req, res);
 });
 
-userRouter.get('/all', checkRole(['user', 'admin']), async (req: Request, res: Response) => {
+userRouter.get('/all', checkRole(['user', 'admin']), async (req: ExpressRequest, res: Response) => {
   await userController.getAllUsers(req, res)
 });
 
-userRouter.get('/one/:id', checkRole(['user', 'admin']), async (req: Request, res: Response) => {
+userRouter.get('/one/:id', checkRole(['user', 'admin']), async (req: ExpressRequest, res: Response) => {
   await userController.getOneUser(req, res);
 });
 
-userRouter.delete('/delete/:id', checkRole(['user', 'admin']), async (req: Request, res: Response) => {
+userRouter.delete('/delete/:id', checkRole(['user', 'admin']), async (req: ExpressRequest, res: Response) => {
   await userController.deleteUser(req, res);
 })
 
