88178ff6a42f86fb5b9ab12734b5351ee157f919
Stephen Oyeyemi <94493468+stevesdiary@users.noreply.github.com>
2025-02-07 19:46:55 +0100

    refactor: reorganize router imports and update routes for hospital, appointment, and doctor modules

--- Server/src/router.ts
+++ Server/src/router.ts
@@ -1,13 +1,16 @@
 import express, { Router } from "express";
 
-import userRouter from "./modules/user/routes/registration.route";
+import userRouter from "./modules/user/routes/user.route";
 import loginRouter from "./modules/user/routes/login.route";
-import providerRouter from "./modules/provider/routes/provider.route";
-
+import hospitalRouter from "./modules/hospital/routes/hospital.route";
+import appointmentRouter from "./modules/appointment/routes/appointment.route";
+import doctorRouter from "./modules/doctor/routes/doctor.route";
 const router = Router();
 
 router.use("/user", userRouter);
 router.use("/log", loginRouter);
-router.use("/provider", providerRouter);
+router.use("/hospital", hospitalRouter);
+router.use('/appointment', appointmentRouter);
+router.use('/doctor', doctorRouter);
 
 export default router;
