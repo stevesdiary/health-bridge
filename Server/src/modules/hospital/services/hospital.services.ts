a8a6c3718c6b88df6e193ba0fe1553e3d9c440e6
Stephen Oyeyemi <94493468+stevesdiary@users.noreply.github.com>
2025-02-11 15:20:04 +0100

    feat: implement CRUD operations for hospital records with improved error handling

--- Server/src/modules/hospital/services/hospital.services.ts
+++ Server/src/modules/hospital/services/hospital.services.ts
@@ -1,7 +1,8 @@
-import {Op as Operators } from 'sequelize';
+import {CreationAttributes, Op as Operators } from 'sequelize';
 import { Hospital } from '../models/hospital.model';
 import { getFromRedis, saveToRedis } from '../../../core/redis';
 import { SearchData } from '../../types/type';
+import { fail } from 'assert';
 
 export const getHospitals = async (searchData: SearchData) => {
   try {
@@ -24,7 +25,7 @@ export const getHospitals = async (searchData: SearchData) => {
           { name: { [Operators.iLike]: searchPattern } },
           { city: { [Operators.iLike]: searchPattern } },
           { state: { [Operators.iLike]: searchPattern } },
-          { specialization: { [Operators.iLike]: searchPattern } },
+          // { specialization: { [Operators.iLike]: searchPattern } },
         ]
       }
     });
@@ -54,9 +55,73 @@ export const getHospitals = async (searchData: SearchData) => {
 
 export const getOneHospital = async (id: string) => {
   try {
-    
+    const hospital = await Hospital.findByPk(id, {
+      attributes: {
+        exclude: ["createdAt", "updatedAt"]
+      }
+    });
+    if (!hospital) {
+      return {
+        statusCode: 404,
+        status: 'fail',
+        message: 'Hospital record not found',
+        data: []
+      }
+    }
+    return {
+      statusCode: 200,
+      status: "success",
+      message: "Hospital record found",
+      data: hospital,
+    };
   } catch (error) {
     console.log('Error showed up', error);
+    throw error;
+  }
+}
+
+export const updateHospital = async (id: string, hospitalData: CreationAttributes<Hospital>) => {
+  try {
+    const hospital = await Hospital.findByPk(id);
+    if (!hospital) {
+      return {
+        statusCode: 404,
+        status: "fail",
+        message: "Hospital not found",
+        data: [],
+      };
+    }
+    const updatedHospital = await hospital.update(hospitalData);
+    return {
+      statusCode: 200,
+      status: "success",
+      message: "Hospital updated",
+      data: updatedHospital,
+    };
+  } catch (error) {
+    throw error;
+  }
+}
+
+export const deleteHospital = async (id: string) => {
+  try {
+    const deletedRow = await Hospital.destroy({ where: { id }});
+    if (!deletedRow || deletedRow === 0) {
+      return {
+        statusCode: 404,
+        status: "fail",
+        message: "Hospital not found",
+        data: [],
+      };
+    }
+
+    return {
+      statusCode: 200,
+      status: "success",
+      message: "Hospital deleted",
+      data: deletedRow,
+    };
+  } catch (error) {
     throw error;
   }
 }
