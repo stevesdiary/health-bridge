f097e0b80942b39e2a6836a3fc6afa93e5ac08c8
Stephen Oyeyemi <94493468+stevesdiary@users.noreply.github.com>
2025-02-11 15:20:41 +0100

    refactor: improve authentication middleware for better token handling and error responses

--- Server/src/middlewares/authentication.ts
+++ Server/src/middlewares/authentication.ts
@@ -9,23 +9,26 @@ const authentication = (req: ExpressRequest, res: Response, next: NextFunction) 
     if (!secret) {
         throw new Error('JWT_SECRET must be defined in environment variables');
     }
-    const token = req.headers.authorization?.split(' ')[1] as string;
-    if (!token) {
-      res.status(401).json({ message: 'No token provided' });
-    }
+		let token;
+		// if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+			token = req.headers.authorization?.split(' ')[1] as string;
+		// }
+		if (!token) {
+				res.status(401).json({ message: 'No token provided' });
+			}
     try {
-        const decoded = jwt.verify(token, secret as string) as JwtPayload;
-        if (!decoded) {
-            res.status(401).json({ message: 'Unauthorized' });
-            return;
-        }
+			const decoded = jwt.verify(token, secret as string) as JwtPayload;
+			if (!decoded) {
+					res.status(401).json({ message: 'Unauthorized' });
+					return;
+			}
 
-        req.user = decoded //as  { id: string; role: UserRole };
-        next();
-    
+			req.user = decoded //as  { id: string; role: UserRole };
+			next();
+
     } catch (error) {
-        console.error('AUTHENTICATION ERROR:', error);
-        res.status(401).send({ error: 'Please authenticate.' });
-    } 
+			console.error('AUTHENTICATION ERROR:', error);
+			res.status(401).send({ error: 'Please authenticate.' });
+    }
 };
 export default authentication;
