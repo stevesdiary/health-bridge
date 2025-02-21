2148df21d67e978537cce8319ab798190b2168f2
Stephen Oyeyemi <94493468+stevesdiary@users.noreply.github.com>
2025-02-21 20:40:42 +0100

    feat: add authentication and role-based access control to appointment routes

--- Server/src/modules/appointment/routes/appointment.route.ts
+++ Server/src/modules/appointment/routes/appointment.route.ts
@@ -2,30 +2,31 @@ import { Router, Request as ExpressRequest, Response } from "express";
 
 import { checkRole } from "../../../middlewares/authorisation";
 import  appointmentController from "../controllers/appointment.controller";
+import authentication from '../../../middlewares/authentication';
 
 const appointmentRouter = Router();
 
-appointmentRouter.post('/create', async (req: ExpressRequest, res: Response) => {
+appointmentRouter.post('/create', authentication, checkRole(['user', 'admin']), async (req: ExpressRequest, res: Response) => {
   await appointmentController.bookAppointment(req, res);
 })
 
-appointmentRouter.get('/all', async (req: ExpressRequest, res: Response) => {
+appointmentRouter.get('/all', authentication, checkRole(['user', 'admin']), async (req: ExpressRequest, res: Response) => {
   await appointmentController.getAppointments(req, res);
 })
 
-appointmentRouter.get('/one', async (req: ExpressRequest, res: Response) => {
+appointmentRouter.get('/one', authentication, checkRole(['user', 'admin']), async (req: ExpressRequest, res: Response) => {
   await appointmentController.getOneAppointmentRecord(req, res);
 })
 
-appointmentRouter.put('/update/:id', async (req: ExpressRequest, res: Response) => {
+appointmentRouter.put('/update/:id', authentication, checkRole(['user', 'admin']), async (req: ExpressRequest, res: Response) => {
   await appointmentController.updateAppointment(req, res);
 })
 
-appointmentRouter.post('/cancel/:id', async (req: ExpressRequest, res: Response) => {
+appointmentRouter.post('/cancel/:id', authentication, checkRole(['user', 'admin']), async (req: ExpressRequest, res: Response) => {
   await appointmentController.cancelAppointment(req, res);
 })
 
-appointmentRouter.delete('/delete', async (req: ExpressRequest, res: Response) => {
+appointmentRouter.delete('/delete', authentication, checkRole(['admin']), async (req: ExpressRequest, res: Response) => {
   await appointmentController.deleteAppointmentRecord(req, res);
 })
 
