55017607aeed22b9201685c3c0e3654dd0538550
Stephen Oyeyemi <94493468+stevesdiary@users.noreply.github.com>
2025-04-10 15:09:32 +0100

    Update database.ts

--- Server/src/core/database.ts
+++ Server/src/core/database.ts
@@ -1,10 +1,12 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import { Sequelize } from 'sequelize-typescript';
-import { User } from '../modules/user/models/user.model';
-import { Appointment } from '../modules/appointment/models/appointment.model';
-import { Hospital } from '../modules/hospital/models/hospital.model';
-import { Doctor } from '../modules/doctor/model/doctor.model';
+import { User } from '../modules/user/user.model';
+import { Appointment } from '../modules/appointment/appointment.model';
+import { Hospital } from '../modules/hospital/hospital.model';
+import { Doctor } from '../modules/doctor/doctor.model';
+import { Patient } from '../modules/patient/patient.model';
+import { Payment } from '../modules/payment/payment.model';
 
 const sequelize = new Sequelize({
   dialect: 'postgres',
@@ -13,7 +15,7 @@ const sequelize = new Sequelize({
   username: process.env.DB_USER || 'postgres',
   password: process.env.DB_PASSWORD || 'postgres',
   database: process.env.DB_NAME || 'your_database',
-  models: [User, Appointment, Hospital, Doctor],
+  models: [User, Appointment, Hospital, Doctor, Patient, Payment],
 });
 
 export default sequelize;
