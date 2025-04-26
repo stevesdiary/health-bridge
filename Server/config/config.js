dc67342e6da054f5a9110376fb9a044bc013b0ac
Stephen Oyeyemi <94493468+stevesdiary@users.noreply.github.com>
2025-04-26 20:25:03 +0100

    Update config for production and test evironmennts

--- Server/config/config.js
+++ Server/config/config.js
@@ -23,10 +23,46 @@ const config = {
     migrationStorageTableName: 'migrations'
   },
   test: {
-    // same configuration as development
+    dialect: 'postgres',
+    dialectModule: require('pg'),
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    ssl: true,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+      sslmode: 'require'
+    },
+    seederStorage: 'sequelize',
+    seederStorageTableName: 'seeders',
+    migrationStorage: 'sequelize',
+    migrationStorageTableName: 'migrations'
   },
   production: {
-    // same configuration as development
+    dialect: 'postgres',
+    dialectModule: require('pg'),
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    ssl: true,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+      sslmode: 'require'
+    },
+    seederStorage: 'sequelize',
+    seederStorageTableName: 'seeders',
+    migrationStorage: 'sequelize',
+    migrationStorageTableName: 'migrations'
   }
 };
 
